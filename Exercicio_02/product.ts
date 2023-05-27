e3c192f72297ab5a6b0235e261a9f78f9b4daba1
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-27 15:17:10 -0300

    creating properties and constructor

---
+++ Exercicio_02/product.ts
@@ -0,0 +1,15 @@
+class Product{
+    code: number;
+    name: string;
+    price: number;
+    quantity: number;
+
+    constructor(code: number, name: string, price: number, quantity: number){
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    
+}
