54724df413369a0e096036063a70aa1ecbf88293
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-28 14:38:08 -0300

    Finishing method remove

--- Exercicio_02/product.ts
+++ Exercicio_02/product.ts
@@ -11,16 +11,10 @@ class Product{
         this.quantity = quantity;
     }
 
-    // totalPrice(price: number, quantity: number){
-    //     this.price = price;
-    //     this.quantity = quantity;
-    //     const total = quantity * price;
-    //     console.log(`O valor total do produto é de ${total}.`)
-    // }
-
     totalPrice(){
-        const total = this.price * this.quantity
-        console.log(`O valor total do produto é de ${total}.`);
+        // const total = this.price * this.quantity
+        // console.log(`O valor total do produto é de ${total}.`);
+        return console.log(`O valor total do produto é de ${this.price * this.quantity}.`);
       }
 }
 
@@ -33,7 +27,14 @@ class Cart{
 
     remove(product: Product){
         const index = this.cart.indexOf(product);
-        this.cart.splice(index);
+        if(index > -1){
+            this.cart.splice(index, 1);
+        }
+        console.log(index)
+    }
+
+    total(){
+
     }
 }
 
@@ -42,12 +43,16 @@ const carrinho = new Cart()
 const product1 = new Product(10, 'camiseta', 20, 2);
 const product2 = new Product(102, 'calça', 10, 2);
 const product3 = new Product(24, 'bermuda', 50, 5);
+const product4 = new Product(39, 'bermuda', 10, 5);
+const product5 = new Product(42, 'bermuda', 20, 2);
 
 carrinho.add(product1);
 carrinho.add(product2);
 carrinho.add(product3);
+carrinho.add(product4);
+carrinho.add(product5);
 console.log(carrinho);
-carrinho.remove(product2)
+carrinho.remove(product3)
 console.log(carrinho);
 
 // console.log(product); // Product { code: 10, name: 'camiseta', price: 20, quantity: 2 }
