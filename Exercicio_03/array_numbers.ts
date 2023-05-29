91bf3148d44ba6d6bbe50ad0abc363b187620ca0
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-29 15:39:28 -0300

    refactoring the code and implements the for

--- Exercicio_03/array_numbers.ts
+++ Exercicio_03/array_numbers.ts
@@ -1,14 +1,16 @@
 function arrayNumbers(array: number[] = []){
     // const newArray: number [] = array.sort(function(a, b){
-    array.sort(function(a, b){
+    
+    const newArray: number[] = [...array];
+    newArray.sort(function(a, b){
         return a - b;
         // 3(a) - 6(b) >> 3(a) - 9(b) >> 3(a) - 1(b)...
         // Precisa retornar negativo se o primeiro item for menor;
         // positivo se for maior, ou zero se forem iguais.
     })
     
-    //console.log(newArray);
     console.log(array);
+    console.log(newArray);
 }
 
 arrayNumbers([3, 6, 9, 1, 2, 2, 4, 7, 10, 5]);
--- Exercicio_03/array_numbers_for.ts
+++ Exercicio_03/array_numbers_for.ts
@@ -1,15 +1,12 @@
 function arrayNumbersFor(array: number[] = []){
-    // const newArray: number [] = array.sort(function(a, b){
-    array.sort(function(a, b){
-        return a - b;
-        // 3(a) - 6(b) >> 3(a) - 9(b) >> 3(a) - 1(b)...
-        // Precisa retornar negativo se o primeiro item for menor;
-        // positivo se for maior, ou zero se forem iguais.
-    })
-    
-    //console.log(newArray);
-    console.log(array);
+    let min: number = array[0];
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] <= min){
+            min = array[i];
+        }
+    }
+    console.log(min);
 }
 
-arrayNumbersFor([3, 6, 9, 1, 2, 2, 4, 7, 10, 5]);
-// result = [ 1, 2, 2, 3, 4, 5, 6, 7, 9, 10];
+arrayNumbersFor([-1, 7, 4, 5, 8, 3, 40, 4, 0, -3]);
