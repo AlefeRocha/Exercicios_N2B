4dd84465bffe031f43ae174a778985155c7bf38c
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-29 19:46:59 -0300

    Adjusting getFriends to show the list of friends

--- Exercicio_04/person.ts
+++ Exercicio_04/person.ts
@@ -14,15 +14,23 @@ class Person{
     }
 
     getFriends(){
-        if(this.friendList.length !== -1){
-            this.friendList.sort((a: Person, b: Person) => {
-                return b.age - a.age;
-            });
-        } else {
-            console.log('Lista de amigos está vazia.');
-        }
+        // return this.friendList.sort((a, b) => b.age - a.age);
+
+        // if(this.friendList.length !== -1){
+        //     this.friendList.sort((a: Person, b: Person) => {
+        //         return b.age - a.age;
+        //     });
+        // } else {
+        //     console.log('Lista de amigos está vazia.');
+        // }
         
         //return this.friendList;
+
+        if(this.friendList.length !== -1){
+            return this.friendList.sort((a, b) => b.age - a.age);
+        } else {
+            return console.log('Lista de amigos está vazia.');
+        }
     }
 }
 
@@ -32,15 +40,17 @@ let pessoa1 = new Person('Alefe', 31);
 let pessoa2 = new Person('Maria', 28);
 let pessoa3 = new Person('Stella', 26);
 let pessoa4 = new Person('Marcos', 30);
+let pessoa5 = new Person('Marcos', 42);
 
 pessoa1.addFriend(pessoa2);
 pessoa1.addFriend(pessoa3);
 pessoa1.addFriend(pessoa4);
+pessoa1.addFriend(pessoa5);
 // pessoa2.addFriend(pessoa1);
 // pessoa2.addFriend(pessoa3);
 
 // console.log(pessoa1.friendList);
-console.log(pessoa2.friendList);
 // console.log(pessoa2.friendList);
 // console.log(pessoa3.friendList);
-console.log(pessoa1.getFriends);
+console.log(pessoa1.getFriends());
+console.log(pessoa2.getFriends());
