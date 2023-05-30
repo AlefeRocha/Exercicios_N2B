61deb4f3fe249af5f06cc4c038b82498f27debfb
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-30 15:14:24 -0300

    Creating the classes

--- Exercicio_05/pessoa.ts
+++ Exercicio_05/pessoa.ts
@@ -1,11 +1,16 @@
-class Pessoa{
-    nome: string;
-    idade: number;
+class Person{
+    name: string;
+    age: number;
     email: string;
 
-    constructor(nome: string, idade: number, email: string){
-        this.nome = nome;
-        this.idade = idade;
+    constructor(name: string, idade: number, email: string){
+        this.name = name;
+        this.age = idade;
         this.email = email;
     }
+}
+
+class Client extends Person{
+    clientID: number;
+    registrationDate: Date;
 }
