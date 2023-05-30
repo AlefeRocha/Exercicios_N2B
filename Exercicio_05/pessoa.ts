feb69f48fcb511959a0f405701073720b669ab94
AlefeRocha <alefe.rtavares@gmail.com>
2023-05-30 16:40:56 -0300

    Finishing the exercise

--- Exercicio_05/pessoa.ts
+++ Exercicio_05/pessoa.ts
@@ -3,9 +3,9 @@ class Person{
     age: number;
     email: string;
 
-    constructor(name: string, idade: number, email: string){
+    constructor(name: string, age: number, email: string){
         this.name = name;
-        this.age = idade;
+        this.age = age;
         this.email = email;
     }
 }
@@ -13,4 +13,25 @@ class Person{
 class Client extends Person{
     clientID: number;
     registrationDate: Date;
+    
+    constructor(name: string, age: number, email: string, clientID: number, registrationDate: Date){
+        super(name, age, email);
+        this.clientID = clientID;
+        this.registrationDate = registrationDate;
+    }
+
+    get dataClient(){
+        return {
+            name: this.name,
+            age: this.age,
+            email: this.email,
+            clientID: this.clientID,
+            registrationDate: this.registrationDate,
+        }
+    }
 }
+
+
+const clienteJoao = new Client('João', 30, 'joao@gmail.com', 2525, new Date('2023-05-30'));
+
+console.log(clienteJoao.dataClient);
