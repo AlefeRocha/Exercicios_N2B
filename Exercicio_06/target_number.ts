e70818235284138d8467882ffe8621b4ff8e62d6
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-01 17:42:58 -0300

    Improving the conditional if

--- Exercicio_06/target_number.ts
+++ Exercicio_06/target_number.ts
@@ -2,17 +2,16 @@ function arrayNumbers(randomArray: Array<number>, targetNumber: number){
     // let array = new Array<number>();
     for(let i = 0; i < randomArray.length; i++){
         const num = targetNumber - randomArray[i];
-        //console.log(num)
-        // if(randomArray.includes(num) === true){
-        //     return 'sim';
-        // } else {
-        //     return 'não';
-        // }
+        //console.log(randomArray[i])
+        if(randomArray.includes(num) === true){
+            const index1: number = randomArray.indexOf(i);
+            const arrayResult: number[] = [index1, randomArray.indexOf(num)];
+            return `Os índices são: ${arrayResult}`;
+        }
     }
 }
 
-// console.log(arrayNumbers([5, 12, 8, 130, 44], 20));
+console.log(arrayNumbers([5, 12, 8, 130, 44], 20));
 
-
-const arr = [5, 12, 8, 130, 44];
-console.log(arr.includes(8));
+// const arr = [5, 12, 8, 130, 44];
+// console.log(arr.includes(8));
