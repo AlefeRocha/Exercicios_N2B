4f9d6710d8ffd1abee5f48fb36737831357f86e9
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-01 13:33:45 -0300

    Using for to iterate the array and find the other number

--- Exercicio_06/target_number.ts
+++ Exercicio_06/target_number.ts
@@ -1,4 +1,9 @@
-function arrayNumbers(){
-    let array = new Array<number>();
+function arrayNumbers(randomArray: Array<number>, targetNumber: number){
+    // let array = new Array<number>();
+    for(let i = 0; i < randomArray.length; i++){
+        const num = targetNumber - randomArray[i];
+    }
+    
 }
 
+console.log(arrayNumbers([5, 12, 8, 130, 44], 20));
