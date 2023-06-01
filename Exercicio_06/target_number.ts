215d622b4c38b3f5c779629e27e97893f7915305
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-01 14:13:48 -0300

    Testing the includes method

--- Exercicio_06/target_number.ts
+++ Exercicio_06/target_number.ts
@@ -2,8 +2,17 @@ function arrayNumbers(randomArray: Array<number>, targetNumber: number){
     // let array = new Array<number>();
     for(let i = 0; i < randomArray.length; i++){
         const num = targetNumber - randomArray[i];
+        //console.log(num)
+        // if(randomArray.includes(num) === true){
+        //     return 'sim';
+        // } else {
+        //     return 'não';
+        // }
     }
-    
 }
 
-console.log(arrayNumbers([5, 12, 8, 130, 44], 20));
+// console.log(arrayNumbers([5, 12, 8, 130, 44], 20));
+
+
+const arr = [5, 12, 8, 130, 44];
+console.log(arr.includes(8));
