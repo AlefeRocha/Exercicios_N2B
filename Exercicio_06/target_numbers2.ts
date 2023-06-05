84050953c77f5efdce90e5ada1fb2b9403fc8dec
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-05 11:13:48 -0300

    Cleaning the comments

--- Exercicio_06/target_numbers2.ts
+++ Exercicio_06/target_numbers2.ts
@@ -4,29 +4,14 @@ function arrayNumbers(randomArray: Array<number>, targetNumber: number){
         const result = targetNumber - currentNumber;
         let arrayResult: number[] = [];
 
-        // console.log('Este é o numero do array no momento: ' + currentNumber);
-        // console.log(`Este é o resultado(${result}) == ${targetNumber} - ${currentNumber}`);
-        console.log('Este é o indice do for ' + i);
-        console.log(`Este é o index do resultado(${randomArray.indexOf(result)}).`);
-        console.log('-------------------------------');
-
         if(randomArray.indexOf(result) != -1 && randomArray.indexOf(result) != i){
             arrayResult = [randomArray.indexOf(result), i];
             return arrayResult.sort((a, b) => a - b);
         }
-
-        // if(randomArray.includes(result) && randomArray.indexOf(result) != randomArray.indexOf(currentNumber)){
-        //     return [randomArray.indexOf(currentNumber), randomArray.indexOf(result)];
-        //     //return [randomArray.indexOf(currentNumber), i];
-        // }
     }
     return []
 }
-// quebrou a solução 👇
+// array que quebrava a solução 👇
 console.log(arrayNumbers([5, 5, 8, 130, 44], 10));
 console.log(arrayNumbers([5, 6, 8, 130, 44], 11));
 console.log(arrayNumbers([5, 12, 8, 130, 44], 52));
-
-// console.log(arrayNumbers([5, 5, 8, 130, 44], 10));
-// console.log(arrayNumbers([5, 6, 8, 130, 44], 11));
-// console.log(arrayNumbers([5, 12, 8, 130, 44], 52));
