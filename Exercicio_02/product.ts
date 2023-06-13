4cdfac3d3435d0726de2c521ddb73a5b861dc4af
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-13 17:10:51 -0300

    Trying to figure out the method total

--- Exercicio_02/product.ts
+++ Exercicio_02/product.ts
@@ -8,7 +8,7 @@ class Product{
         }
 }
 
-// function getPrice(code: number){
+// function getPrice(code: number): number{
 //     let productPrice = this.items.get(code.price);
 //     return productPrice;
 // }
@@ -18,7 +18,7 @@ class Cart{
     
     add(product: Product, quantity: number): void{
         let added = this.items.get(product.code)
-        console.log(added);
+        console.log(typeof added);
         if(added){
             this.items.set(product.code, added + quantity)
             return
@@ -49,7 +49,6 @@ class Cart{
 
             console.log(code);
             console.log(quantity);
-            console.log(totalPrice);
             totalCart += totalPrice * quantity;
         }
         console.log(`O valor total do carrinho é: R$${totalCart}.`);
@@ -59,8 +58,8 @@ class Cart{
 
 
 
-// instanciando
-const cart = new Cart()
+// Instanciando
+const cart = new Cart();
 const product1 = new Product(111, 'Camiseta', 20);
 const product2 = new Product(222, 'Blusa', 10);
 // const product3 = new Product(24, 'Bermuda', 50, 5);
@@ -72,7 +71,6 @@ cart.add(product1, 5);
 cart.add(product2, 10);
 // cart.remove(200, 2);
 console.log(cart);
-console.log(product1.price);
 cart.total();
 
 
