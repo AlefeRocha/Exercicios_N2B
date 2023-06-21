69318ea180e9d652eb201f7f14a0c83074dcae05
AlefeRocha <alefe.rtavares@gmail.com>
2023-06-21 17:04:45 -0300

    Refactoring the code

--- Exercicio_01/bankAccount.ts
+++ Exercicio_01/bankAccount.ts
@@ -26,24 +26,3 @@ class BankAccount {
         console.log(`Seu saldo é de R$${this.balance}.`);
     }
 }
-
-
-// Instanciando
-const account = new BankAccount();
-
-account.deposit(100);
-account.deposit(20);
-account.withdraw(70);
-account.withdraw(200);
-
-// Log
-console.log(account.historic);
-account.viewBalance();
-
-// Resultados
-// Depósito de R$100 feito. Saldo de R$100.
-// Depósito de R$20 feito. Saldo de R$120.
-// Saque de R$70 realizado! Saldo de R$50.
-// O valor do saque de R$200, é maior que o seu saldo de R$50.
-// [ 100, 20, -70 ]
-// Seu saldo é de R$50.
